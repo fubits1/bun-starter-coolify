8e18e58f359b567fea9178dfbb92fac9db6498d2
fubits1 <ilja_s@protonmail.com>
2024-03-08 11:19:57 +0100

    feat: add source

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 const PORT = process.env.PORT || 3000;
 Bun.serve({
-    port: PORT,
-    fetch(req) {
-        const url = new URL(req.url);
-        if (url.pathname === "/") return new Response("Home page!");
-        if (url.pathname === "/blog") return new Response("Blog!");
-        return new Response("404!");
-    },
-})
+  port: PORT,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === '/')
+      return new Response(
+        'Home page! Source: https://github.com/coollabsio/coolify-examples/tree/main/bun'
+      );
+    if (url.pathname === '/blog') return new Response('Blog!');
+    return new Response('404!');
+  },
+});
 console.log(`Server running at http://localhost:${PORT}`);
